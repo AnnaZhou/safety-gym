07cf6d9ac21b10f0cbff7307bc1951a7e73be9bf
annazhou <zn.anna@gmail.com>
2024-11-17 08:25:59 -0800

    make changes to new mujoco

--- safety_gym/envs/engine.py
+++ safety_gym/envs/engine.py
@@ -850,7 +850,9 @@ class Engine(gym.Env, gym.utils.EzPickle):
         print("goal_body_id:",goal_body_id)
         #self.sim.model.body_pos[goal_body_id][:2] = self.sim.layout['goal']
         self.sim.model.body_pos[goal_body_id][:2] = self.layout['goal']
-        print(self.sim.model.body_pos[goal_body_id][:2])
+        print("body_pos:",self.sim.model.body_pos[goal_body_id][:2])
+        print("model:",self.model.geom_id2name)
+        print("sim.model:",self.sim.model.body_pos)
         #self.sim.forward()
         mujoco.mj_forward(self.model, self.data)
         print("qpos:", self.data.qpos)
