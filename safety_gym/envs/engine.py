9efbe00667e5a309cecdc8e64f64e4a12cfb1d3d
annazhou <zn.anna@gmail.com>
2024-11-18 10:47:29 -0800

    make changes to new mujoco

--- safety_gym/envs/engine.py
+++ safety_gym/envs/engine.py
@@ -975,7 +975,7 @@ class Engine(gym.Env, gym.utils.EzPickle):
         # Get a render context so we can
         rows, cols = self.vision_size
         width, height = cols, rows
-        #vision = self.sim.render(width, height, camera_name='vision', mode='offscreen')
+        vision = self.sim.render(width, height, camera_name='vision', mode='offscreen')
         return np.array(vision, dtype='float32') / 255
 
     def obs_lidar(self, positions, group):
